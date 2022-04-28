1aca56436d12db841e29bb3db9039ba5c218a726
sangzun-han <hsjun1996@naver.com>
2022-04-28 18:08:46 +0900

    fix : untracked files

--- 백준/재귀/10870.py
+++ 백준/재귀/10870.py
@@ -1,11 +1,11 @@
 def fibo(n):
-  if n==0:
-    return 0
-  if n==1:
-    return 1
-  else:
-    return fibo(n-2) + fibo(n-1)
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fibo(n-2) + fibo(n-1)
+
 
 n = int(input())
 print(fibo(n))
-
--- 백준/재귀/10872.py
+++ 백준/재귀/10872.py
@@ -1,10 +1,7 @@
 # 팩토리얼
 
 def fact(n):
-  if n > 0:
-    return n * fact(n-1)
-  else:
-    return 1
-
-
-
+    if n > 0:
+        return n * fact(n-1)
+    else:
+        return 1
