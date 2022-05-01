46fda7dd9dc403b499e99ce6ebfd8b06c3b0649a
sangzun-han <hsjun1996@naver.com>
2022-05-01 22:00:46 +0900

    11650

---
+++ 백준/정렬/11650.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    x, y = map(int, sys.stdin.readline().split())
+    arr.append((x, y))
+
+arr.sort()
+
+for i in range(n):
+    print(arr[i][0], arr[i][1])
