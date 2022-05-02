f341fb9939dfd4c6c87689efe3fa751eb1ea1384
sangzun-han <hsjun1996@naver.com>
2022-05-02 21:55:10 +0900

    10989

--- 백준/정렬/10989.py
+++ 백준/정렬/10989.py
@@ -1,1 +1,32 @@
-print(round(7/2))
+import sys
+
+n = int(sys.stdin.readline().strip())
+lists = [0] * 10001
+
+for i in range(n):
+    lists[int(sys.stdin.readline())] += 1
+
+for i in range(len(lists)):
+    if lists[i] != 0:
+        for j in range(lists[i]):
+            print(i)
+
+
+'''
+메모리 초과
+import sys
+
+n = int(sys.stdin.readline().strip())
+data = []
+for i in range(n):
+  value = int(sys.stdin.readline().strip())
+  data.append(value)
+count = [0] * (max(data) + 1)
+
+for i in range(len(data)):
+  count[data[i]] += 1
+
+for i in range(len(count)):
+  for j in range(count[i]):
+    print(i)
+'''
