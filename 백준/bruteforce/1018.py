38bab13e167eb8956f5f2c2a379b4c8e16f1e986
sangzun-han <hsjun1996@naver.com>
2022-05-02 21:53:57 +0900

    1018

--- 백준/bruteforce/1018.py
+++ 백준/bruteforce/1018.py
@@ -1,26 +1,30 @@
-N = 8
-str1 = "WBWBWBWB"
-str2 = "BWBWBWBW"
-chess1 = [str1, str2, str1, str2, str1, str2, str1, str2]
-chess2 = [str2, str1, str2, str1, str2, str1, str2, str1]
-result = float('inf')
+import sys
 
-n, m = [int(x) for x in input().split()]
-arr = []
-for i in range(n):
-    arr.append(input())
-for i in range(n-N+1):
-    for j in range(m-N+1):
+white_board = ['WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW',
+               'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW']
+
+black_board = ['BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB',
+               'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB']
+
+data = []
+result = 100000
+n, m = map(int, sys.stdin.readline().split())
+
+for _ in range(n):
+    data.append(input())
+
+for i in range(n-8+1):
+    for j in range(m-8+1):
         count = 0
-        for k in range(N):
-            for z in range(N):
-                if arr[i+k][j+z] != chess1[k][z]:
+        for k in range(8):
+            for l in range(8):
+                if data[i+k][j+l] != white_board[k][l]:
                     count += 1
         result = min(result, count)
         count = 0
-        for k in range(N):
-            for z in range(N):
-                if arr[i+k][j+z] != chess1[k][z]:
+        for k in range(8):
+            for l in range(8):
+                if data[i+k][j+l] != black_board[k][l]:
                     count += 1
         result = min(result, count)
 
